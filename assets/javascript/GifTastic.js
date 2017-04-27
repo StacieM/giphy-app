c74858a3f23eef25abe5a5eb541e529ec8aaeef3
Stacie Meierhoefer <staciemead@gmail.com>
2017-04-26 21:17:06 -0700

    working API pull

--- assets/javascript/GifTastic.js
+++ assets/javascript/GifTastic.js
@@ -1,45 +1,43 @@
+$(document).ready(function() {
 
-
-var topics = ["Michael Jordan", "Roger Federer", "Wimbledon", "Tommy Lasorda", "Lionel Messi"];
-
-  // need to make div or other container to display gif's
-
+var topics = ["Soccer", "Baseball", "Surfing", "Tennis", "Golf"];
   // Function for displaying topics data
 function renderButtons() {
-
         // Deleting the topic buttons prior to adding new topic buttons
         // (this is necessary otherwise we will have repeat buttons)
-    $("#topics-view").empty();
-
+  $("#topics-view").empty();
         // Looping through the array of topics
-    for (var i = 0; i < topics.length; i++) {
-        // Then dynamicaly generating buttons for each topic in the array.
-        // This code $("<button>") is all jQuery needs to create the start and end tag. (<button></button>)
-        var a = $("<button>");
-        // Adding a class
-        a.addClass("topic");
-        // Adding a data-attribute with a value of the topic at index i
-        a.attr("data-name", topics[i]);
-        // Providing the button's text with a value of the topic at index i
-        a.text(topics[i]);
-        // Adding the button to the HTML
-        $("#topics-view").append(a);
-        }
-      }
-
-      $("#add-topic").on("click", function(event) {
+  for (var i = 0; i < topics.length; i++) {
+    $('#topics-view').append("<button class='sports' data-sport=" + topics[i] + ">" + topics[i] + "</button>");
+    }
+};
+  $("#add-topic").on("click", function(event) {
         // event.preventDefault() prevents the form from trying to submit itself.
         // We're using a form so that the user can hit enter instead of clicking the button if they want
-        event.preventDefault();
-
+    event.preventDefault();
         // This line will grab the text from the input box
-        var topic = $("#topic-input").val().trim();
+    var topic = $("#topic-input").val().trim();
         // The topic from the textbox is then added to our array
-        topics.push(topic);
-
+    topics.push(topic);
         // calling renderButtons which handles the processing of our topic array
-        renderButtons();
-      });
+    renderButtons();
+  });
+  renderButtons();
+  $("button").on("click", function() {
+    var x =  $(this).data("sport");
+    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + x + "&api_key=dc6zaTOxFJmzC&limit=10";
+    $.ajax({url:queryURL, method: "GET"})
+    .done(function(response){
+      for(var i=0; i<response.data.length; i++){
+        var sportDiv = $('<div>');
+        var p = $('<p>').text("Rating: "+response.data[i].rating);
+        var sportImage = $('<img>');
+        sportImage.attr('src',response.data[i].images.fixed_height.url);
+        sportDiv.append(p);
+        sportDiv.append(sportImage);
+        $('#gifsGoHere').append(sportDiv);
+      }
+    });
+  })
+})
 
-      // Calling the renderButtons function at least once to display the initial list of topics
-      renderButtons();
